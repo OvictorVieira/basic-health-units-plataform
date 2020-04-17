3e79cc5ac28391a38a504cc76911ebf6c332047f
victor vieira <victor.h.souza.vieira@gmail.com>
2020-04-17 17:34:57 -0300

    configure webpack to build in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
+    path: __dirname + '/public/assets/',
+    publicPath: "/assets/",
+    filename: "bundle.js"
   }
 }
