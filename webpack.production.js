292970e7c76bfa37a5958d03d79cbb7c357d0e35
Victor Hugo Vieira <victor.h.souza.vieira@gmail.com>
2020-05-19 15:18:37 -0300

    adds production Dotenv parameter to read heroku variables

--- webpack.production.js
+++ webpack.production.js
@@ -1,7 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
    mode: 'production',
-   devtool: 'source-map'
+   devtool: 'source-map',
+   plugins: [
+      new webpack.DefinePlugin({
+         'process.env.REACT_APP_GOOGLE_MAP_URL': JSON.stringify(process.env.REACT_APP_GOOGLE_MAP_URL),
+         'process.env.REACT_APP_GOOGLE_API_KEY': JSON.stringify(process.env.REACT_APP_GOOGLE_API_KEY),
+         'process.env.REACT_APP_API_HOST': JSON.stringify(process.env.REACT_APP_API_HOST),
+         'process.env.REACT_APP_PRIVATE_KEY': JSON.stringify(process.env.REACT_APP_PRIVATE_KEY),
+      })
+   ]
 });
